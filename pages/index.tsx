4dea929a2c05b4fee4a593e85e8ae5c983ce0411
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-06-29 14:23:34 +0900

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+
+const Home: NextPage = () => {
+  const [showText, setShowText] = useState(false)
+  const onClick = () => setShowText(true)
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <button data-testid="Button" onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white text-4xl font-bold py-4 px-8 rounded">Click</button>
+        { showText ? <p data-testid="Text" className="text-4xl pt-10 text-center">Hello</p> : null }
+      </div>
+    </div>
+  )
+}
+
+export default Home
